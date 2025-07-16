a234c166f3ad61c73c4ca26ac748018df660059a
Vox1dov <voxidov.j.n@gmail.com>
2025-07-16 18:23:40 +0500

    Update and rename README.md to lesson-1/homework/hw1.1.sql

---
+++ lesson-1/homework/hw1.1.sql
@@ -0,0 +1,33 @@
+Easy
+Ma'lumotlar-Real dunyodagi faktlar, raqamlar yoki belgilar to‘plami, xom axborot shaklida bo‘ladi.
+Ma'lumotlar bazasi-Ma'lumotlarni tartib bilan saqlash, boshqarish va ularga tezkor kirishni ta'minlovchi tizim.
+Relyatsion ma'lumotlar bazasi-Ma'lumotlarni jadval shaklida saqlaydigan ma'lumotlar bazasi turi.
+Jadvallar o‘zaro bog‘langan bo‘lib, har bir jadval ustunlar (atributlar) va satrlardan (yuzalar) tashkil topgan.
+Relyatsion ma'lumotlar bazasidagi ma'lumotlarni saqlash shakli, ustunlar va satrlardan iborat.
+Har bir ustun ma'lum bir ma'lumot turini ifodalaydi, har bir satr esa bitta yozuv hisoblanadi.
+
+2. SQL Serverning beshta asosiy xususiyati:
+Yuqori ishlash samaradorligi — katta hajmdagi ma'lumotlarni tez va samarali boshqaradi.
+Yuqori xavfsizlik darajasi — ma'lumotlarni himoya qilish uchun kuchli autentifikatsiya va ruxsat tizimlari mavjud.
+Zaxira nusxa olish va tiklash imkoniyatlari — ma'lumotlarni yo‘qotishdan saqlash uchun qulay vositalar bor.
+Transaktsiyalarni qo‘llab-quvvatlash — ma'lumotlarning yaxlitligini ta'minlaydigan ACID xususiyatlari.
+Kengaytirilgan tahlil va hisobot vositalari — ma'lumotlarni tahlil qilish uchun integratsiyalashgan vositalar (SQL Server Reporting Services va boshqalar).
+
+3. SQL Serverga ulanishda autentifikatsiya usullari:
+Windows Authentication (Windows autentifikatsiyasi)
+Foydalanuvchi Windows hisobiga asoslanib tizimga ulanadi. Bu usul ko‘proq xavfsiz va qulay hisoblanadi
+SQL Server Authentication (SQL Server autentifikatsiyasi)
+Maxsus foydalanuvchi nomi va parol bilan SQL Serverga ulanadi. Bu usul Windows hisobidan mustaqil ishlaydi.
+
+Middle
+create database SchoolDB
+Use SchoolDB
+create table Students (studentID int, Name varchar(50), Age int)
+select * from Students
+
+ SQL Server-Microsoft tomonidan ishlab chiqilgan ma'lumotlar bazasi boshqaruv tizimi (DBMS).Ma'lumotlar bazasini yaratish, saqlash, boshqarish va ularga murojaat qilish.             
+ SSMS-SQL Server uchun grafik interfeysli boshqaruv dasturi (GUI).SQL Serverni boshqarish, so‘rov yozish, ma'lumotlar bazasini vizual boshqarish.           
+ SQL-Ma'lumotlar bazasini boshqarish uchun ishlatiladigan til(Structured Query Language).Ma'lumotlarni yaratish, o‘zgartirish, so‘rov qilish va boshqarish uchun buyruqlar yozish. 
+
+
+
