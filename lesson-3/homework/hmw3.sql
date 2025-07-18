0f279b04bf9f4046322689ae4fb2d5905ca12198
Vox1dov <voxidov.j.n@gmail.com>
2025-07-18 09:20:08 +0500

    Create hmw3.sql

---
+++ lesson-3/homework/hmw3.sql
@@ -0,0 +1,17 @@
+Oson
+1)BULK INSERT — bu SQL Serverga tashqi fayldan katta hajmdagi ma’lumotlarni jadvalga tezda import qilish uchun ishlatiladigan buyruqdir. U ko‘pincha CSV yoki txt fayllardagi ma’lumotlarni yuklashda ishlatiladi. 2).CSV – Comma Separated Values
+
+  2).CSV – Comma Separated Values
+.TXT – Text fayl
+.XML – Extensible Markup Language
+.JSON – JavaScript Object Notation
+  
+3)create table Products (ProductID INT PRIMARY KEY, ProductName VARCHAR(50), Price DECIMAL(10,2))
+
+4)insert into Products (ProductID, ProductName, Price) values (1,'Olma', 35.50),(2,'Nok', 40.00),(3,'Uzum', 30.80) 
+
+5) Nut-Ma'lumotlar aniq emas yoki noma'lum, qiymati aniq emas,ixtiyoriy ustunda qo'llanadi.
+  Not Null-Ma'lumotlar aniq, qiymati bo'lishi majburiy, majburiy to'ldiriladigan ustunda qo'llaniladi.
+
+  6)
+
